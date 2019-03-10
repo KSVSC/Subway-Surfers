3b4b3e030806d0199b2f0a731358f90d0e246c79
KSVSC <samhitaexplorica9@gmail.com>
2019-03-10 15:06:07 +0530

    Game World

--- trail.js
+++ trail.js
@@ -1,8 +1,8 @@
-var scene,camera,renderer,mesh;
-var meshFloor, ambientLight, light;
+var scene,camera,renderer, composer , mesh;
+var meshFloor, ambientLight, light, poster1, poster2;
 
 var keyboard = {};
-var player = { height: 1.0, speed: 0.5, turnSpeed: 0.5};
+var player = { height: 1.0, speed: 1.5, zspeed: 0.5, yspeed:0.1, turnSpeed: 0.5};
 var USE_WIREFRAME = false;
 
 var loadingScreen = {
@@ -17,8 +17,16 @@ var loadingScreen = {
 
 var LOADING_MANAGER = null;
 var RESOURCES_LOADED = false;
-var Jump = 0,Duck = 0;
-var time, time2,timed, timed2;
+var Jump = 0, Duck = 0;
+var Boot_time = 2;
+var time, time2, timed, timed2;
+var Fly_Over = 0;
+var Boot_Jump = 0;
+var Gray = 0;
+var timef1, timef2;
+var x = 0;
+var score = 0;
+var alive = 1;
 
 //models index
 var models = {
@@ -38,10 +46,15 @@ var models = {
         mtl: "./Track.mtl",
         mesh: null        
     },
-    coin: {
+    coin1: {
         obj: "./coinGold.obj",
         mtl: "./coinGold.mtl",
         mesh: null        
+    },
+    coin2: {
+        obj: "./coinSilver.obj",
+        mtl: "./coinSilver.mtl",
+        mesh: null
     },
     flag: {
         obj: "./flag.obj",
@@ -52,15 +65,68 @@ var models = {
         obj: "./police/bb8.obj",
         mtl: "./police/bb8.mtl",
         mesh: null        
+    },
+    dog: {
+        obj: "./cartDog.obj",
+        mtl: "./cartDog.mtl",
+        mesh: null
+    },
+    obj1: {
+        obj: "./overheadRoundColored.obj",
+        mtl: "./overheadRoundColored.mtl",
+        mesh: null
+    },
+    tree: {
+        obj: "./treeLarge.obj",
+        mtl: "./treeLarge.mtl",
+        mesh: null 
+    },
+    obj2: {
+        obj: "./barrierWall.obj",
+        mtl: "./barrierWall.mtl",
+        mesh: null 
+    },
+    tree: {
+        obj: "./treeLarge.obj",
+        mtl: "./treeLarge.mtl",
+        mesh: null
+    },
+    pole: {
+        obj: "./Lightpost_01.obj",
+        mtl: "./Lightpost_01.mtl",
+        mesh: null
+    },
+    flyover: {
+        obj: "./button.obj",
+        mtl: "./button.mtl",
+        mesh: null
+    },
+    boot: {
+        obj: "./boot/21943_Mushroom_v1.obj",
+        mtl: "./boot/21943_Mushroom_v1.mtl",
+        mesh: null
+    },
+    train: {
+        obj: "./train/814_4.obj",
+        mtl: "./train/814_4.mtl",
+        mesh: null
     }
     
 };
 
 //mesh index
 var meshes = {};
-var coins = {};
+var coin1 = {};
+var coin2 = {};
 var world = {};
 var End = {};
+var obs1 = {};
+var Tree = {};
+var obs2 = {};
+var Pole = {};
+var Fly = {};
+var Boot = {};
+var Train = {};
 
 function init() {
     scene = new THREE.Scene();
@@ -82,6 +148,7 @@ function init() {
         onResourcesLoaded();
     };
 
+
     //loading textures
     var textureLoader = new THREE.TextureLoader(loadingManager);
     mesh_texture = textureLoader.load("wall.jpg");
@@ -90,57 +157,45 @@ function init() {
 
     //floor
     meshFloor = new THREE.Mesh(
-        new THREE.PlaneGeometry(20, 500, 10, 10),
+        new THREE.PlaneGeometry(20, 1200, 10, 10),
         new THREE.MeshPhongMaterial({
-            color: 0xffffff,
+            color: 0x000fff,
             // wireframe: USE_WIREFRAME,
             map: mesh_texture
         })
     );
     meshFloor.rotation.x -= Math.PI / 2;
     meshFloor.receiveShadow = true;
-    meshFloor.position.set(0, 0, 250);
+    meshFloor.position.set(0, 0, 600);
     scene.add(meshFloor);
 
     //wall1
     poster1 = new THREE.Mesh(
-        new THREE.BoxGeometry(0.9, 5, 400),
+        new THREE.BoxGeometry(0.9, 5, 1000),
         new THREE.MeshPhongMaterial({
             color: 0xffffff,
             wireframe: USE_WIREFRAME,
-            map: floor_texture
+            map: floor_texture,
+            transparent:true
         })
     );
     poster1.receiveShadow = true;
-    poster1.position.set(4.5, 2.5, 200);
+    poster1.position.set(5.5, 2.5, 500);
     scene.add(poster1);
 
     //wall2
     poster2 = new THREE.Mesh(
-        new THREE.BoxGeometry(0.9, 5, 400),
+        new THREE.BoxGeometry(0.9, 5, 1000),
         new THREE.MeshPhongMaterial({
             color: 0xffffff,
             wireframe: USE_WIREFRAME,
-            map: floor_texture
+            map: floor_texture,
+            transparent: true
         })
     );
     poster2.receiveShadow = true;
-    poster2.position.set(-4, 2.5, 200);
-    scene.add(poster2);
-
-    //obstacle1
-    obstacle1[i] = new THREE.Mesh(
-        new THREE.BoxGeometry(0.9, 5, 400),
-        new THREE.MeshPhongMaterial({
-            color: 0xffffff,
-            wireframe: USE_WIREFRAME,
-            map: floor_texture
-        })
-    );
-    obstacle1[i].receiveShadow = true;
-    obstacle1[i].position.set(4.5, 2.5, 200);
-    scene.add(obstacle1[i]);
-        
+    poster2.position.set(-5, 2.5, 500);
+    scene.add(poster2);        
 
     ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
     scene.add(ambientLight);
@@ -151,6 +206,7 @@ function init() {
     light.shadow.camera.near = 0.1;
     light.shadow.camera.far = 25;
     scene.add(light);
+    
 
     //load models
     for (var _key in models) {
@@ -193,6 +249,16 @@ function init() {
 
     document.body.appendChild(renderer.domElement);
 
+
+    //grayscale
+    composer = new THREE.EffectComposer(renderer);
+    var renderPass = new THREE.RenderPass(scene, camera);
+    composer.addPass(renderPass);
+
+    var effectGrayScale = new THREE.ShaderPass(THREE.LuminosityShader);
+    effectGrayScale.renderToScreen = true;
+    composer.addPass(effectGrayScale);   
+
     animate(); 
 
 }
@@ -200,27 +266,50 @@ function init() {
 function onResourcesLoaded() {
     Surfer = models.player.mesh.clone();
     Police = models.police.mesh.clone();
+    Dog = models.dog.mesh.clone();
+    for (i = 0; i < 2; i++) {
+        Train[i] = models.train.mesh.clone();
+    }
+    for (i = 0; i < 2; i++){
+        Fly[i] = models.flyover.mesh.clone();
+    }
+    for (i = 0; i < 2; i++) {
+        Boot[i] = models.boot.mesh.clone();
+    }
+    for (i = 0; i < 6; i++){
+        obs1[i] = models.obj1.mesh.clone();
+    }
+    for (i = 0; i < 16; i++) {
+        Tree[i] = models.tree.mesh.clone();
+    }
+    for (i = 0; i < 16; i++) {
+        Pole[i] = models.pole.mesh.clone();
+    }
+    for (i = 0; i < 10; i++) {
+        obs2[i] = models.obj2.mesh.clone();
+    }
     for (i = 0; i < 3; i++){
         End[i] = models.flag.mesh.clone();        
     }
-    for (i = 0; i < 10 * 4; i = i + 4) {
+    for (i = 0; i < 25 * 4; i = i + 4) {
         world[i] = models.wall.mesh.clone();
         world[i + 1] = models.wall.mesh.clone();
         world[i + 2] = models.track.mesh.clone();
         world[i + 3] = models.track.mesh.clone();
     }
-    for (i = 0; i < 2000; i++)
+    for (i = 0; i < 1000; i++)
     {
-        coins[i] = models.coin.mesh.clone();
+        coin1[i] = models.coin1.mesh.clone();
+        coin2[i] = models.coin2.mesh.clone();
     }
 
 
-    for (i = 0, j = 30, k = 0; i < 10 * 4; i = i + 4){
-        world[i].position.set(-4, 0, j);
+    for (i = 0, j = 30, k = 0; i < 25 * 4; i = i + 4){
+        world[i].position.set(-5, 0, j);
         scene.add(world[i]);
         world[i].scale.set(1, 2, 10);
         
-        world[i + 1].position.set(4, 0, j);
+        world[i + 1].position.set(5, 0, j);
         scene.add(world[i+1]);
         world[i + 1].scale.set(1, 2, 10);
         
@@ -235,30 +324,181 @@ function onResourcesLoaded() {
         j += 40; 
     }
 
-    Surfer.position.set(0, 0, 10);
-    scene.add(Surfer);
     Surfer.scale.set(0.015, 0.015, 0.015);
+    Surfer.position.set(-1.54, 0, 10);
+    scene.add(Surfer);
 
     Police.position.set(1, 0, 5);
-    // scene.add(Police);
+    scene.add(Police);
     Police.rotation.y -= Math.PI / 2;
     Police.scale.set(0.015, 0.013, 0.007);
 
-    for (i = 0, x = -1, y = 0.01, z = 10; i < 200; i++){
-        if (i == 15) {
-            z = 125;
-            x = 2;
-            y = 2;
-        }
-        coins[i].position.set(x, y, z);
-        coins[i].scale.set(1, 1, 1);
-        coins[i].rotation.y = Math.PI / 2;
-        scene.add(coins[i]);
-        z += 3;
+    Dog.position.set(-2, 1, 5);
+    scene.add(Dog);
+    Dog.scale.set(0.15, 0.1, 0.002);
+
+    // Train[0].position.set(2.25, 0, 95);
+    // Train[1].position.set(-1.54, 0, 620);
+    // for (i = 0; i < 2; i++) {
+    //     scene.add(Train[i]);
+    //     Train[i].scale.set(0.25, 0.25, 1);
+    // }
+
+    Fly[0].position.set(-2.5, 0, 112);
+    Fly[1].position.set(0.5, 0, 510);
+    for (i = 0; i < 2; i++){
+        scene.add(Fly[i]);
+        Fly[i].scale.set(2, 3, 1);
+    }
+
+    Boot[0].position.set(-1.2, 0, 308);
+    Boot[1].position.set(1.5, 0, 812);
+    for (i = 0; i < 2; i++) {
+        Boot[i].scale.set(0.1, 0.1, 0.1);
+        Boot[i].rotation.x -= Math.PI / 2;
+        scene.add(Boot[i]);
+    }
+
+    for (i = 0, j = 20; i < 6; i++){
+        obs1[i].position.set(3.9, 0.001,j);
+        obs1[i].scale.set(4, 3, 1);
+        scene.add(obs1[i]);
+        j = j + 100;
+    }
+
+    for (i = 0, j = 15; i < 16; i++) {
+        Tree[i].position.set(0, 0.001, j);
+        Tree[i].scale.set(2, 4, 1);
+        scene.add(Tree[i]);
+
+        j = j + 50;
+    }
+
+    for (i = 0, j = 100; i < 10; i++) {
+        if (i % 2 == 0)
+            x = -0.55;
+        else
+            x = 2.55;
+        obs2[i].position.set(x, 0.001, j);
+        obs2[i].scale.set(2, 7, 1);
+        scene.add(obs2[i]);
+        j = j + 80;
+    }
+
+    for (i = 0, j = 40; i < 16; i++) {
+        Pole[i].scale.set(2, 5, 1);
+        Pole[i].position.set(-0.55, 0.001, j);
+        scene.add(Pole[i]);
+
+        j = j + 50;
+    }
+
+    for (i = 0,z=10; i < 15; i++){
+        coin1[i].position.set(-1.2, 0, z);
+        coin1[i].scale.set(1, 1, 1);
+        coin1[i].rotation.y = Math.PI / 2;
+        scene.add(coin1[i]);
+
+        coin2[i].position.set(-1.2, 0, z + 3);
+        coin2[i].scale.set(1, 1, 1);
+        coin2[i].rotation.y = Math.PI / 2;
+        scene.add(coin2[i]);
+        z += 6;
+    }
+
+    for (i = 15, z = 120; i < 15 + 15; i++) {
+        coin1[i].position.set(2, 0, z);
+        coin1[i].scale.set(1, 1, 1);
+        coin1[i].rotation.y = Math.PI / 2;
+        scene.add(coin1[i]);
+
+        coin2[i].position.set(2, 0, z + 3);
+        coin2[i].scale.set(1, 1, 1);
+        coin2[i].rotation.y = Math.PI / 2;
+        scene.add(coin2[i]);
+        z += 6;
+    }
+
+    for (i = 30, z = 250; i < 15 + 30; i++) {
+        coin1[i].position.set(-1.2, 1.5, z);
+        coin1[i].scale.set(1, 1, 1);
+        coin1[i].rotation.y = Math.PI / 2;
+        scene.add(coin1[i]);
+
+        coin2[i].position.set(-1.2, 1.5, z + 3);
+        coin2[i].scale.set(1, 1, 1);
+        coin2[i].rotation.y = Math.PI / 2;
+        scene.add(coin2[i]);
+        z += 6;
+    }
+
+    for (i = 45, z = 400; i < 15 + 45; i++) {
+        coin1[i].position.set(2, 1.5, z);
+        coin1[i].scale.set(1, 1, 1);
+        coin1[i].rotation.y = Math.PI / 2;
+        scene.add(coin1[i]);
+
+        coin2[i].position.set(2, 1.5, z + 3);
+        coin2[i].scale.set(1, 1, 1);
+        coin2[i].rotation.y = Math.PI / 2;
+        scene.add(coin2[i]);
+        z += 6;
+    }
+
+    for (i = 60, z = 570; i < 15 + 60; i++) {
+        coin1[i].position.set(-1.2, 2, z);
+        coin1[i].scale.set(1, 1, 1);
+        coin1[i].rotation.y = Math.PI / 2;
+        scene.add(coin1[i]);
+
+        coin2[i].position.set(-1.2, 2, z + 3);
+        coin2[i].scale.set(1, 1, 1);
+        coin2[i].rotation.y = Math.PI / 2;
+        scene.add(coin2[i]);
+        z += 6;
+    }
+
+    for (i = 75, z = 820; i < 15 + 75; i++) {
+        coin1[i].position.set(2, 0, z);
+        coin1[i].scale.set(1, 1, 1);
+        coin1[i].rotation.y = Math.PI / 2;
+        scene.add(coin1[i]);
+
+        coin2[i].position.set(2, 0, z + 3);
+        coin2[i].scale.set(1, 1, 1);
+        coin2[i].rotation.y = Math.PI / 2;
+        scene.add(coin2[i]);
+        z += 6;
+    }
+
+    for (i = 90, z = 110; i < 15 + 90; i++) {
+        coin1[i].position.set(-1.2, 5, z);
+        coin1[i].scale.set(1, 1, 1);
+        coin1[i].rotation.y = Math.PI / 2;
+        scene.add(coin1[i]);
+
+        coin2[i].position.set(-1.2, 5, z + 3);
+        coin2[i].scale.set(1, 1, 1);
+        coin2[i].rotation.y = Math.PI / 2;
+        scene.add(coin2[i]);
+        z += 6;
+    }
+
+    for (i = 115, z = 500; i < 15 + 115; i++) {
+        coin1[i].position.set(2, 5, z);
+        coin1[i].scale.set(1, 1, 1);
+        coin1[i].rotation.y = Math.PI / 2;
+        scene.add(coin1[i]);
+
+        coin2[i].position.set(2, 5, z + 3);
+        coin2[i].scale.set(1, 1, 1);
+        coin2[i].rotation.y = Math.PI / 2;
+        scene.add(coin2[i]);
+        z += 6;
     }
 
     for (i = 0,j=-1; i < 3; i=i+1){
-        End[i].position.set(j, 0, 380);
+        End[i].position.set(j, 0, 980);
         End[i].scale.set(10, 3, 1);
         End[i].rotation.y = Math.PI / 2;        
         scene.add(End[i]);
@@ -280,18 +520,55 @@ function animate() {
     }
     
     requestAnimationFrame(animate);
-    if (Jump == 1) {
-        time2 = Date.now() * 0.0005;
-        delta = time2 - time;
-
-        if (delta < 0.5) {
-            Surfer.position.y += 0.1;
+    if (x < 5) {
+        x = x + 1;
+    }
+    else {
+        if (poster1.material.opacity == 1) {
+            poster1.material.opacity = 0.8;
+            poster2.material.opacity = 0.8;            
         }
         else {
-            Surfer.position.y -= 0.1;
+            poster1.material.opacity = 1;
+            poster2.material.opacity = 1;
         }
-        if (Surfer.position.y < 0)
+        x = 0;
+    }
+    
+    if (Fly_Over == 1) {
+
+        timef2 = Date.now() * 0.0005;
+        delta3 = timef2 - timef1;
+        if (delta3 < 3){
+            Surfer.position.y = 5;                
+        }
+        else {
+            Surfer.position.y = 0;
+            Fly_Over = 0;
+        }
+    }
+
+    if (Jump == 1 || Boot_Jump ==1) {
+        time2 = Date.now() * 0.0005;
+        delta = time2 - time;
+        if (Boot_Jump) {
+            Jump_time = 0.5;
+        }
+        else
+        {
+            Jump_time = 0.1;
+        }
+        if (delta < Jump_time) {
+            Surfer.position.y += player.yspeed;
+        }
+        else {
+            Surfer.position.y -= player.yspeed;
+        }
+        if (Surfer.position.y < 0) {
+            Boot_Jump = 0;
             Jump = 0;
+
+        }
     }
     if (Duck == 1) {
         timed2 = Date.now() * 0.0005;
@@ -306,8 +583,8 @@ function animate() {
         }
     }
 
-    if (Surfer.position.z < 380){
-        Surfer.position.z += player.speed;        
+    if (Surfer.position.z < 980){
+        Surfer.position.z += player.zspeed;
         Police.position.z += player.turnSpeed;
         player.turnSpeed -= 0.001;
     }
@@ -315,11 +592,19 @@ function animate() {
     camera.position.set(Surfer.position.x, Surfer.position.y + 3, Surfer.position.z - 25);
     camera.lookAt(new THREE.Vector3(Surfer.position.x, Surfer.position.y + 1, Surfer.position.z));
     
+    Dog.position.z = Surfer.position.z - 5;
+    Dog.position.x = Surfer.position.x - 1;
+    if (Surfer.position.y < 2)
+        Dog.position.y = Surfer.position.y + 1;
+    else
+        Dog.position.y = 1;
     
+
     //key functions
     if (keyboard[87]) {//Wkey
         console.log("key W")
-
+        Fly_Over = 1;
+        timef1 = Date.now() * 0.0005;
         camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
         camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
     }
@@ -331,7 +616,9 @@ function animate() {
     }
     if (keyboard[65]) {//Akey
         console.log("key A")
-
+        Boot_Jump = 1;
+        time = Date.now() * 0.0005;
+        Surfer.position.y += player.yspeed;
         camera.position.x += Math.sin(camera.rotation.y + Math.PI / 2) * player.speed;
         camera.position.z += -Math.cos(camera.rotation.y + Math.PI / 2) * player.speed;
     }
@@ -343,23 +630,23 @@ function animate() {
     }
     if (keyboard[37]) {//left arrow
         console.log("key left")
-        if (Surfer.position.x + player.speed < 3 && Surfer.position.x + player.speed > -2.5) {
+        if (Surfer.position.x + player.speed < 2 && Surfer.position.x + player.speed > -2.5) {
             Surfer.position.x += player.speed;            
         }
-        // camera.rotation.y -= player.turnSpeed;
+        camera.rotation.y -= player.turnSpeed;
     }
     if (keyboard[39]) {//right arrow
         console.log("right")
-        if (Surfer.position.x - player.speed < 3 && Surfer.position.x - player.speed > -2.5) {
+        if (Surfer.position.x - player.speed < 2 && Surfer.position.x - player.speed > -2.5) {
             Surfer.position.x -= player.speed;            
         }
-        // camera.rotation.y += player.turnSpeed;
+        camera.rotation.y += player.turnSpeed;
     }
     if (keyboard[32]) {//spacebar
         console.log("space")
         Jump = 1;
         time = Date.now() * 0.0005;
-        Surfer.position.y += 0.1;
+        Surfer.position.y += player.yspeed;
     }
     if (keyboard[40]) {//Downarrow
         console.log("duck")
@@ -367,8 +654,18 @@ function animate() {
         time = Date.now() * 0.0005;
         Surfer.scale.set(0.01, 0.01, 0.01);
     }
+    if (keyboard[90]) {//Downarrow
+        console.log("grayscale")
+        if (Gray == 0)
+            Gray = 1;
+        else
+            Gray = 0;
+    }
 
-    renderer.render(scene, camera);    
+    if (Gray)
+        composer.render();
+    else
+        renderer.render(scene, camera);    
 }
 
 function keyDown(event) {
@@ -384,5 +681,4 @@ function keyUp(event) {
 
 window.addEventListener('keydown', keyDown);
 window.addEventListener('keyup', keyUp);
-
 window.onload = init;
